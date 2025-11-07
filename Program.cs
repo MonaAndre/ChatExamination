9553297f5b9a7ef33f8b9b8a5a615368a137c313
Mona <monaelizabet@gmail.com>
2025-11-07 18:21:56 +0100

    seted up project

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ChatExample;
+
+// ws är protokollet som står för websocket
+// wss är samma sak som https, alltså TLS/SSL aktiverad.
+/*
+ *
+ * Händelser/Events skickas mellan klienten och servern.
+ * Varje event har ett eventName och en data-innehåll.
+ *
+ * Vi väljer själva vilka event namn som vi vill lyssna på samt skicka.
+ * Vi väljer själva vilken datastruktur samt vilka events som vi ska ha.
+ *
+ *
+ */
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Vi ansluter till Socket servern.
+        await SocketManager.Connect();
+        
+        static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        static string ReadInput()
+        {
+            string userMessage = Console.ReadLine();
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            ClearCurrentConsoleLine();
+            return userMessage;
+        }
+
+        while (true)
+        {
+
+            await SocketManager.SendMessage(ReadInput());
+        }
+
+    }
+}
---
+++ SocketManager.cs
@@ -0,0 +1,65 @@
+namespace ChatExample;
+
+using SocketIOClient;
+
+public class SocketManager
+{
+    private static SocketIO _client;
+    private static readonly string Path = "/sys25d";
+    public static List<string> messages;
+    
+    // Här kan vi välja ett unikt event namn för meddelanden.
+    private static readonly string EventName = "monas_chat";
+    
+    static SocketManager()
+    {
+        messages = [];
+    }
+    
+    // Här ska vi ansluta till socketio servern.
+    public static async Task Connect()
+    {
+        // Vi skapar en instans av SocketIO och konfigurerar den med
+        // våran server url och path.
+        _client = new SocketIO("wss://api.leetcode.se", new SocketIOOptions
+        {
+            Path = Path
+        });
+        
+        // Här nedan anger vi de events vi vill lyssna på
+        // samt en handler för varje event som ska köras.
+        _client.On(EventName, response =>
+        {
+            string receivedMessage = response.GetValue<string>();
+            
+            Console.WriteLine($"Received message: {receivedMessage}");
+        });
+
+        // Kod vi kan köra när vi etablerar en anslutning
+        _client.OnConnected += (sender, args) =>
+        {
+            Console.WriteLine("Connected!");
+        };
+        
+        // Kod vi kan köra när vi tappar anslutningen
+        _client.OnDisconnected += (sender, args) =>
+        {
+            Console.WriteLine("Disconnected!");
+        };
+
+        
+        await _client.ConnectAsync();
+        
+        // Vi lägger en fördröjning på 2000ms (2s) för att se till att klienten har anslutit och satt upp allt.
+        await Task.Delay(2000);
+        
+        Console.WriteLine($"Connected: {_client.Connected}");
+    }
+
+    // Skicka meddelanden.
+    public static async Task SendMessage(string message)
+    {
+        await _client.EmitAsync(EventName, message);
+        Console.WriteLine($"You said: {message}");
+    }
+}
