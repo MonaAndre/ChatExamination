8e313981ec6bd0a7e2d3a72e59bed5c1a9015abf
Mona <monaelizabet@gmail.com>
2025-11-14 12:55:32 +0100

    Feat: Added typing logic Refactor: refactoring code, formatting and corrected namegiving

--- Chat.cs
+++ Chat.cs
@@ -16,21 +16,47 @@ public class Chat
 
         static string ReadInput()
         {
-            string userMessage = Console.ReadLine();
+            string userMessage = "";
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Backspace && userMessage.Length > 0)
+                {
+                    userMessage = userMessage.Substring(0, userMessage.Length - 1);
+                    Console.Write("\b \b");
+                }
+                else if (key.Key != ConsoleKey.Enter && key.Key != ConsoleKey.Backspace)
+                {
+                    userMessage += key.KeyChar;
+                    Console.Write(key.KeyChar);
+                    SocketManager.NotifyTyping();
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
             Console.SetCursorPosition(0, Console.CursorTop - 1);
             ClearCurrentConsoleLine();
+            SocketManager.NotifyStopTyping();
             return userMessage;
         }
 
-        bool isChatting = true;
+
+        var isChatting = true;
         while (isChatting)
         {
-            string userInputText = ReadInput();
+            var userInputText = ReadInput();
             if (userInputText == "/quit")
             {
                 Console.Clear();
                 await SocketManager.DisconnectAsync();
                 isChatting = false;
+            }
+            else if (string.IsNullOrWhiteSpace(userInputText))
+            {
+                Console.Write("You can not send a empty message, to continue chat press enter");
+                userInputText = ReadInput();
             }
             else
             {
